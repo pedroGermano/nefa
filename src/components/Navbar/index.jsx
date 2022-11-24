adaf9413d398abffc1ea23ba3d421f4aa7a709b8
Pedro Germano <pedrogermano232@gmail.com>
2022-11-24 17:27:00 -0300

    folder images add

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Logo from '../../images/logo.svg';
 
 export default function Navbar() {
   return (
     <div className='py-4 flex'>
       <div className='py-4 flex'>
         <div className="flex">
-          
+
         </div>
       </div>
       <div></div>
